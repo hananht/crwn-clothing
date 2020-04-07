c52712305022adcf5f86fe4d1fba9742480ffe81
hananht <hananht@gmail.com>
2020-04-07 23:26:39 +0300

    Added sign in page with custom form input components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
+
+import Header from './components/header/header.component';
 
 import HomePage from './pages/home/home.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import SignPage from './pages/sign/sign.component';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/sign' component={SignPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign/sign.component.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './sign.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+class SignPage extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className='sign'>
+                <SignIn></SignIn>
+            </div>
+        )
+    }
+}
+
+export default SignPage;
